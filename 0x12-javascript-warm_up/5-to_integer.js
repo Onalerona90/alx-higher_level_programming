695c7827f188c5b152143cf13061bfd415c656ff
onalerona <onapako90@gmail.com>
2023-11-13 16:21:50 +0200

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+5-to_integer.js
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
+}
